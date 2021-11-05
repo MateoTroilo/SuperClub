dc6cb7c3cb24a5805d666b9787cd646910e8ab35
MarcoAR <marcorivero.mr26@gmail.com>
2021-11-05 12:08:29 -0300

    definir rutas

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('assets'))
 app.set('view engine', 'ejs')
 
-
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (_, res) => res.render('index'))
+app.get('/cart', (_, res) => res.render('cart'))
+app.get('/checkout', (_, res) => res.render('checkout'))
+app.get('/contact', (_, res) => res.render('contact'))
+app.get('/product:id', (req, res) => res.render('product'))
+app.get('/login', (_, res) => res.render('login'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
