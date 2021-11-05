60b56114dc4378d1fccddac72d522152001233e3
MarcoAR <marcorivero.mr26@gmail.com>
2021-11-05 12:32:16 -0300

    rutas corregidas y agregamos productos

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express')
+const productos = require('./productos.js')
 const app = express()
 const port = 3000
 
 app.use(express.static('assets'))
 app.set('view engine', 'ejs')
 
-app.get('/', (_, res) => res.render('index'))
-app.get('/cart', (_, res) => res.render('cart'))
-app.get('/checkout', (_, res) => res.render('checkout'))
-app.get('/contact', (_, res) => res.render('contact'))
-app.get('/product:id', (req, res) => res.render('product'))
-app.get('/login', (_, res) => res.render('login'))
+app.get('/', (_, res) => res.render('pages/index'))
+app.get('/cart', (_, res) => res.render('pages/cart'))
+app.get('/checkout', (_, res) => res.render('pages/checkout'))
+app.get('/contact', (_, res) => res.render('pages/contact'))
+app.get('/product/:id', (req, res) => {
+  const { id } = req.params
+  res.render('pages/product', { id, productos })
+})
+app.get('/login', (_, res) => res.render('pages/login'))
+app.get('/register', (_, res) => res.render('pages/register'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
