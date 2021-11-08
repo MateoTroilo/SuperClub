98624c3ee401ac7cfee430e2b14655453549140f
Mateo Troilo <mateo.troilo@alumnos.frgp.utn.edu.ar>
2021-11-08 14:31:27 -0300

    inicio us7

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const productos = require('./productos.js')
-const app = express()
-const port = 3000
+const express = require("express");
+const productos = require("./productos.js");
+const app = express();
+const port = 3000;
 
-app.use(express.static('assets'))
-app.set('view engine', 'ejs')
+app.use(express.static("assets"));
+app.set("view engine", "ejs");
 
-app.get('/', (_, res) => res.render('pages/index'))
-app.get('/cart', (_, res) => res.render('pages/cart'))
-app.get('/checkout', (_, res) => res.render('pages/checkout'))
-app.get('/contact', (_, res) => res.render('pages/contact'))
-app.get('/product/:id', (req, res) => {
-  const { id } = req.params
-  res.render('pages/product', { id, productos })
-})
-app.get('/login', (_, res) => res.render('pages/login'))
-app.get('/register', (_, res) => res.render('pages/register'))
+app.get("/", (_, res) => res.render("pages/index"));
+app.get("/cart", (_, res) => res.render("pages/cart", { productos }));
+app.get("/checkout", (_, res) => res.render("pages/checkout"));
+app.get("/contact", (_, res) => res.render("pages/contact"));
+app.get("/product/:id", (req, res) => {
+    const { id } = req.params;
+    res.render("pages/product", { id, productos });
+});
+app.get("/login", (_, res) => res.render("pages/login"));
+app.get("/register", (_, res) => res.render("pages/register"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- productos.js
+++ productos.js
@@ -1,2 +1,18 @@
-const data = [{}]
-module.exports = data
+const data = [
+    {
+        img: "./img/havanna2.jpg",
+        name: "Alfajores Havanna Chocolate 12 Unidades",
+        price: 19900,
+    },
+    {
+        img: "./img/mostaza1.jpg",
+        name: "1 Combo Mega Cuarto Deluxe en Mostaza",
+        price: 10000,
+    },
+    {
+        img: "./img/plataforma101.jpg",
+        name: "Voucher de $400 en Plataforma 10",
+        price: 11500,
+    },
+];
+module.exports = data;
