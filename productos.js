0f5fcc7d60c6ddd633e4a5d1d7b897f9e044978f
Mateo Troilo <mateo.troilo@alumnos.frgp.utn.edu.ar>
2021-11-08 17:32:16 -0300

    Bonus agregado

--- productos.js
+++ productos.js
@@ -17,41 +17,5 @@ const data = [
         name: "Voucher de $400 en Plataforma 10",
         price: 11500,
     },
-    {
-        id: 1,
-        img: "./img/havanna2.jpg",
-        name: "Alfajores Havanna Chocolate 12 Unidades",
-        price: 19900,
-    },
-    {
-        id: 2,
-        img: "./img/mostaza1.jpg",
-        name: "1 Combo Mega Cuarto Deluxe en Mostaza",
-        price: 10000,
-    },
-    {
-        id: 3,
-        img: "./img/plataforma101.jpg",
-        name: "Voucher de $400 en Plataforma 10",
-        price: 11500,
-    },
-    {
-        id: 1,
-        img: "./img/havanna2.jpg",
-        name: "Alfajores Havanna Chocolate 12 Unidades",
-        price: 19900,
-    },
-    {
-        id: 2,
-        img: "./img/mostaza1.jpg",
-        name: "1 Combo Mega Cuarto Deluxe en Mostaza",
-        price: 10000,
-    },
-    {
-        id: 3,
-        img: "./img/plataforma101.jpg",
-        name: "Voucher de $400 en Plataforma 10",
-        price: 11500,
-    },
 ];
 module.exports = data;
