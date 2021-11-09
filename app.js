32f4ad057f32512231a53a2ba8fb8d8a54805603
AgusB1101 <agustinbernal69@gmail.com>
2021-11-09 19:49:13 -0300

    Agrego página de Not Found

--- app.js
+++ app.js
@@ -9,22 +9,23 @@ app.use(express.static("assets"));
 app.set("view engine", "ejs");
 
 app.get("/product/:id", (req, res) => {
-    const { id } = req.params;
-    res.render("pages/product", { id, productos });
+  const { id } = req.params;
+  res.render("pages/product", { id, productos });
 });
 app.get("/cart", (_, res) => res.render("pages/cart", { productos }));
 app.get("/checkout", (_, res) => res.render("pages/checkout"));
 app.get("/contact", (_, res) => res.render("pages/contact"));
 app.get("/login", (_, res) => res.render("pages/login"));
 app.get("/register", (_, res) => res.render("pages/register"));
+app.get("/notfound", (_, res) => res.render("pages/notfound", { productos }));
 app.get("/", (_, res) =>
-    res.render("pages/index", {
-        teinteresan: productos.slice(0, 4),
-        lomaspedido: productos,
-        image: "img/profile-pic.png",
-        name: "marco",
-        heros,
-    })
+  res.render("pages/index", {
+    teinteresan: productos.slice(0, 4),
+    lomaspedido: productos,
+    image: "img/profile-pic.png",
+    name: "marco",
+    heros,
+  })
 );
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
