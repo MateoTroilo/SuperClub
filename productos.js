b2e8ad71fc1720dcedba153eeb3b651825022c7e
Mateo Troilo <mateo.troilo@alumnos.frgp.utn.edu.ar>
2021-11-09 18:40:43 -0300

    merge s1-us7

--- productos.js
+++ productos.js
@@ -1,21 +1,21 @@
 const productos = [
     {
-        id: 2,
+        id: 1,
         name: "1 Combo Mega Cuarto Deluxe en Mostaza",
         img: "/img/mostaza1.jpg",
-        price: "10.000",
+        price: 10000,
     },
     {
-        id: 1,
+        id: 2,
         name: "Alfajores Havanna Chocolate 12 un.",
         img: "/img/havanna1.jpg",
-        price: "19.900",
+        price: 19900,
     },
     {
         id: 3,
         name: "Voucher de $400 en Plataforma 10",
         img: "/img/plataforma101.jpg",
-        price: "11.500",
+        price: 11500,
     },
 ];
 
