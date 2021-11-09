fd068bdd14ef581c5d8052a6e476fda7fba1e956
nicobuiatti <nbuiatti@santandertecnologia.com.ar>
2021-11-09 17:13:09 -0300

    productID div arreglado

--- productos.js
+++ productos.js
@@ -1,10 +1,68 @@
-const data = [
-    { id: 1, name: "hola que hace", img: "", price: 12341 },
-    { id: 14, name: "hola que hace", img: "", price: 12341 },
-    { id: 13, name: "hola que hace", img: "", price: 12341 },
-    { id: 12, name: "hola que hace", img: "", price: 12341 },
-    { id: 11, name: "hola que hace", img: "", price: 12341 },
-    { id: 17, name: "hola que hace", img: "", price: 12341 },
-    { id: 18, name: "hola que hace", img: "", price: 12341 },
+const productos = [
+    {
+        id: 1,
+        name: "1 Combo Mega Cuarto Deluxe en Mostaza",
+        img: "/img/mostaza1.jpg",
+        price: "10.000",
+    },
+    {
+        id: 14,
+        name: "Alfajores Havanna Chocolate 12 un.",
+        img: "/img/havanna1.jpg",
+        price: "19.900",
+    },
+    {
+        id: 13,
+        name: "Voucher de $400 en Plataforma 10",
+        img: "/img/plataforma101.jpg",
+        price: "11.500",
+    },
+    {
+        id: 12,
+        name: "NOMBRE DE PRODUCTO O SERVICIO",
+        img: "/img/not_found.png",
+        price: "XX.XXX",
+    },
+    {
+        id: 11,
+        name: "NOMBRE DE PRODUCTO O SERVICIO",
+        img: "/img/not_found.png",
+        price: "XX.XXX",
+    },
+    {
+        id: 17,
+        name: "NOMBRE DE PRODUCTO O SERVICIO",
+        img: "/img/not_found.png",
+        price: "XX.XXX",
+    },
+    {
+        id: 18,
+        name: "NOMBRE DE PRODUCTO O SERVICIO",
+        img: "/img/not_found.png",
+        price: "XX.XXX",
+    },
+    {
+        id: 19,
+        name: "NOMBRE DE PRODUCTO O SERVICIO",
+        img: "/img/not_found.png",
+        price: "XX.XXX",
+    },
 ];
-module.exports = data;
+
+const heros = [
+    {
+        id: 44,
+        background: "/img/disneybg.jpg",
+        icon: "/img/disneybrand.png",
+        title: "50% OFF en Combo Plus",
+        description: "Con tu Tarjeta Santander y 40.000 puntos SuperClub",
+    },
+    {
+        id: 54,
+        background: "/img/hbobg.jpg",
+        icon: "/img/hbobrand.png",
+        title: "50% OFF en HBO Max",
+        description: "Con tu Tarjeta Santander y 50.000 puntos SuperClub",
+    },
+];
+module.exports = { productos, heros };
