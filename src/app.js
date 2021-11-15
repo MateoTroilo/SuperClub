4be69d5598c87e3ab2b59d7ef53bc97dac33bb97
magui <magui.cerisola@gmail.com>
2021-11-15 16:41:52 -0300

    validaciones register

--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
 const express = require('express')
 const mainRoutes = require('./routes/mainRoutes')
+const { check, validationResult, body } = require("express-validator");
+const registerRoutes = require("./routes/registerRoutes");
 
 const app = express()
 const PORT = 3000
 
 console.clear()
 
+app.set("view engine", "ejs");
+
+//validacion
+app.use(express.urlencoded());
+app.use(express.json());
+
+
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.set('views', 'src/views')
-
+app.use("/register", registerRoutes)
 app.use('/', mainRoutes)
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
