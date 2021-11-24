b1c079866fff42e2e63860fbec2ce45514a62c78
MarcoAR <marcorivero.mr26@gmail.com>
2021-11-24 11:22:35 -0300

    fix anchor in menu user

--- public/js/main.js
+++ public/js/main.js
@@ -1,47 +1,63 @@
-const buttonProfile = document.querySelector(".btn-profile");
-const menuUser = document.querySelector(".menuUserContainer");
-const main = document.querySelector("main");
-const changeTheme = document.querySelector(".changeTheme");
-const lightMode = document.querySelector(".toLightMode");
-const darkMode = document.querySelector(".toDarkMode");
+const buttonProfile = document.querySelector('.btn-profile')
+const menuUser = document.querySelector('.menuUserContainer')
+const main = document.querySelector('main')
+const changeTheme = document.querySelector('.changeTheme')
+const lightMode = document.querySelector('.toLightMode')
+const darkMode = document.querySelector('.toDarkMode')
+const links = document.querySelectorAll('.menuUserLinks div')
+const logout = document.querySelector('#logout')
 
-buttonProfile.addEventListener("click", (e) => {
-  menuUser.classList.toggle("show");
+buttonProfile?.addEventListener('click', (e) => {
+  menuUser.classList.toggle('show')
 
   if (window.innerWidth <= 414) {
-    menuUser.focus();
-    main.style.pointerEvents = "none";
-    main.style.filter = "brightness(0.4)";
-    main.style.backgroundColor = "#666";
+    menuUser.focus()
+    main.style.pointerEvents = 'none'
+    main.style.filter = 'brightness(0.4)'
+    main.style.backgroundColor = '#666'
   }
-});
+})
 
-menuUser.addEventListener("click", (e) => {
-  e.stopPropagation();
-});
+links?.forEach((link) => {
+  link?.addEventListener('click', () => {
+    if (link.id === 'inicio') return (window.location.href = '/')
+    if (link.id === 'miscompras') return (window.location.href = '/cart')
+    window.location.href = '#'
+    console.log('me reclickeraon crak')
+  })
+})
 
-menuUser.addEventListener("blur", () => {
-  menuUser.classList.remove("show");
-  main.style.pointerEvents = "initial";
-  main.style.filter = "";
-  main.style.backgroundColor = "initial";
-});
+logout?.addEventListener('click', () => {
+  window.location.href = '/logout'
+})
 
-changeTheme.addEventListener("click", () => {
-  toggleTheme();
-});
+menuUser?.addEventListener('click', (e) => {
+  e.stopPropagation()
+})
+
+menuUser?.addEventListener('blur', () => {
+  console.log('mega blurbeadi')
+  menuUser.classList.remove('show')
+  main.style.pointerEvents = 'initial'
+  main.style.filter = ''
+  main.style.backgroundColor = 'initial'
+})
+
+changeTheme?.addEventListener('click', () => {
+  toggleTheme()
+})
 
 const toggleTheme = () => {
-  if (main.classList.contains("dark")) {
-    lightMode.style.display = "flex";
-    darkMode.style.display = "none";
-    localStorage.setItem("temaPreferido", "dark");
+  if (main.classList.contains('dark')) {
+    lightMode.style.display = 'flex'
+    darkMode.style.display = 'none'
+    localStorage.setItem('temaPreferido', 'dark')
   } else {
-    lightMode.style.display = "none";
-    darkMode.style.display = "flex";
-    localStorage.setItem("temaPreferido", "light");
+    lightMode.style.display = 'none'
+    darkMode.style.display = 'flex'
+    localStorage.setItem('temaPreferido', 'light')
   }
 
-  console.log(localStorage.getItem("temaPreferido"));
-  main.classList.toggle("dark");
-};
+  console.log(localStorage.getItem('temaPreferido'))
+  main.classList.toggle('dark')
+}
