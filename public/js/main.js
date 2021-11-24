05f420c8e62d1f37e26ef43606b85e9ea5b8b2a7
MarcoAR <marcorivero.mr26@gmail.com>
2021-11-24 14:56:46 -0300

    finish us3

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-const buttonProfile = document.querySelector('.btn-profile')
 const menuUser = document.querySelector('.menuUserContainer')
 const main = document.querySelector('main')
 const changeTheme = document.querySelector('.changeTheme')
@@ -6,15 +5,39 @@ const lightMode = document.querySelector('.toLightMode')
 const darkMode = document.querySelector('.toDarkMode')
 const links = document.querySelectorAll('.menuUserLinks div')
 const logout = document.querySelector('#logout')
+const modalBlack = document.querySelector('.modalBlack')
+const footer = document.querySelector('footer')
+const header = document.querySelector('header')
+const productCard = document.querySelectorAll('.productCard')
+const menuUserSettings = document.querySelectorAll('.menuUserSettings img')
+const cart = document.querySelector('.cart')
+const mainCartAnchors = document.querySelectorAll('.main-cart a')
+const cartBtns = document.querySelectorAll('.button-cart')
+const cantBars = document.querySelectorAll('.cant-bar')
+const buttonProfile = document.querySelector('.btn-profile')
+const productMobile = document.querySelector('.product-mobile')
+const btnAddCart = document.querySelectorAll('.btn-agregar-cart')
+const rutaArnchor = document.querySelectorAll('.ruta a')
+const productDesktop = document.querySelector('.producto')
+const body = document.querySelector('body')
+const loginContainer = document.querySelector('.login-container')
+const loginContainerInput = document.querySelectorAll('.login-container input')
+const loginContainerButton = document.querySelector('.login-container button')
+const anchorQuestion = document.querySelector('.question a')
+const registerContainer = document.querySelector('.register')
+const anchorRegisterQuestion = document.querySelector('main a')
+const registerContainerInput = document.querySelectorAll('.register input')
+const registerContainerButton = document.querySelector('.register button')
 
-buttonProfile?.addEventListener('click', (e) => {
+buttonProfile?.addEventListener('click', () => {
   menuUser.classList.toggle('show')
-
   if (window.innerWidth <= 414) {
     menuUser.focus()
+    modalBlack.classList.add('showModalBlack')
+    modalBlack.style.width = `${window.innerWidth}px`
+    modalBlack.style.height = `${window.innerHeight}px`
+
     main.style.pointerEvents = 'none'
-    main.style.filter = 'brightness(0.4)'
-    main.style.backgroundColor = '#666'
   }
 })
 
@@ -38,9 +61,8 @@ menuUser?.addEventListener('click', (e) => {
 menuUser?.addEventListener('blur', () => {
   console.log('mega blurbeadi')
   menuUser.classList.remove('show')
+  modalBlack.classList.remove('showModalBlack')
   main.style.pointerEvents = 'initial'
-  main.style.filter = ''
-  main.style.backgroundColor = 'initial'
 })
 
 changeTheme?.addEventListener('click', () => {
@@ -49,15 +71,71 @@ changeTheme?.addEventListener('click', () => {
 
 const toggleTheme = () => {
   if (main.classList.contains('dark')) {
-    lightMode.style.display = 'flex'
-    darkMode.style.display = 'none'
-    localStorage.setItem('temaPreferido', 'dark')
-  } else {
-    lightMode.style.display = 'none'
-    darkMode.style.display = 'flex'
+    lightMode && (lightMode.style.display = 'none')
+    darkMode && (darkMode.style.display = 'flex')
     localStorage.setItem('temaPreferido', 'light')
+  } else {
+    lightMode && (lightMode.style.display = 'flex')
+    darkMode && (darkMode.style.display = 'none')
+    localStorage.setItem('temaPreferido', 'dark')
   }
-
   console.log(localStorage.getItem('temaPreferido'))
-  main.classList.toggle('dark')
+  const isDark = main.classList.toggle('dark-main')
+  menuUserSettings.forEach((img) => {
+    img.style.filter = isDark
+      ? `invert(100%) sepia(0%) saturate(4097%) hue-rotate(120deg)
+    brightness(111%) contrast(101%)`
+      : ''
+  })
+  cart?.classList.toggle('dark-cart')
+  footer.classList.toggle('dark-footer')
+  header.classList.toggle('dark-header')
+  productCard?.forEach((product) => {
+    product?.classList.toggle('dark-article')
+  })
+  mainCartAnchors?.forEach((anchor) => {
+    anchor.style.color = isDark ? 'white' : 'black'
+  })
+  const children = cart ? [...cart.children] : []
+  children?.forEach((child) => {
+    if (child.tagName !== 'P' && child.tagName !== 'DIV')
+      child.classList.toggle('dark-article')
+
+    if (child.tagName === 'A') child.style.color = 'white'
+  })
+  cantBars?.forEach((btn) => {
+    btn.style.color = 'black'
+  })
+  cartBtns?.forEach((button) => {
+    button.style.backgroundColor = isDark ? 'rgb(236,0,0)' : 'rgb(73,79,81)'
+  })
+  productMobile?.classList.toggle('dark-article')
+  btnAddCart?.forEach((btn) => {
+    btn.style.backgroundColor = '#222'
+  })
+
+  rutaArnchor?.forEach((anchor) => {
+    anchor.style.color = isDark ? 'white' : 'black'
+  })
+  body.style.backgroundColor = isDark ? '#222' : '#efefef'
+  productDesktop?.classList.toggle('dark-article')
+  loginContainer?.classList.toggle('dark-article')
+  registerContainer?.classList.toggle('dark-article')
+  anchorRegisterQuestion &&
+    (anchorRegisterQuestion.style.color = isDark ? 'white' : 'black')
+  anchorQuestion && (anchorQuestion.style.color = isDark ? 'white' : 'black')
+  loginContainerButton &&
+    (loginContainerButton.style.backgroundColor = isDark ? '#222' : '#494F51')
+  registerContainerButton &&
+    (registerContainerButton.style.backgroundColor = isDark
+      ? '#222'
+      : '#494F51')
+  loginContainerInput?.forEach((input) => {
+    input.style.backgroundColor = isDark ? '#494f51' : 'white'
+  })
+  registerContainerInput?.forEach((input) => {
+    input.style.backgroundColor = isDark ? '#494f51' : 'white'
+  })
+
+  menuUser?.classList.toggle('dark-menu')
 }
