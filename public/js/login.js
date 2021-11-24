2e3a88de367011c8dbad64335262a0f05abb6b10
MarcoAR <marcorivero.mr26@gmail.com>
2021-11-24 18:28:02 -0300

    styles input login

--- public/js/login.js
+++ public/js/login.js
@@ -3,6 +3,7 @@ let loginContainer = document.querySelector(".login-container");
 let  modalGrey = document.querySelector(".modal-grey");
 let loginEmail = document.querySelector("#login-email");
 let warningLogin = document.querySelector(".login-warning")
+let contenedorInputLogin = document.querySelector(".contenedor-input-login")
 
 
 loginContainer?.addEventListener("submit", (e) =>{
@@ -18,17 +19,17 @@ modalGrey.addEventListener("click", ()=>{
 loginEmail.addEventListener("click", () =>{
     warningLogin.innerHTML="incompleted"
     warningLogin.style.color="red"
-    loginEmail.classList.add("input-login-warning")
+    contenedorInputLogin.classList.add("contenedor-input-warning")
 })
 
 loginEmail.addEventListener("keyup", () => {
     if ((/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/).test(loginEmail.value) === true){
         warningLogin.innerHTML = "great!"
         warningLogin.style.color = "green"
-        loginEmail.classList.remove("input-login-warning")
+        contenedorInputLogin.classList.remove("contenedor-input-warning")
     } else {
         warningLogin.innerHTML = "invalid"
         warningLogin.style.color = "red"
-        loginEmail.classList.add("input-login-warning")
+        contenedorInputLogin.classList.add("contenedor-input-warning")
     }
 })
