d0b7f7162c4dc13ddc445644f0d2e041c47e5129
AgusB1101 <agustinbernal69@gmail.com>
2021-11-24 10:31:29 -0300

    Agrego cierre de sesión y localStorage para tema oscuro/claro

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,47 @@
-const buttonProfile = document.querySelector('.btn-profile')
-const menuUser = document.querySelector('.menuUserContainer')
-const main = document.querySelector('main')
+const buttonProfile = document.querySelector(".btn-profile");
+const menuUser = document.querySelector(".menuUserContainer");
+const main = document.querySelector("main");
+const changeTheme = document.querySelector(".changeTheme");
+const lightMode = document.querySelector(".toLightMode");
+const darkMode = document.querySelector(".toDarkMode");
 
-buttonProfile.addEventListener('click', (e) => {
-  menuUser.classList.toggle('show')
-  menuUser.focus()
+buttonProfile.addEventListener("click", (e) => {
+  menuUser.classList.toggle("show");
 
   if (window.innerWidth <= 414) {
-    main.style.pointerEvents = 'none'
-    main.style.filter = 'brightness(0.4)'
-    main.style.backgroundColor = '#666'
+    menuUser.focus();
+    main.style.pointerEvents = "none";
+    main.style.filter = "brightness(0.4)";
+    main.style.backgroundColor = "#666";
   }
-})
+});
 
-menuUser.addEventListener('click', (e) => {
-  e.stopPropagation()
-  console.log('clock')
-})
+menuUser.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
 
-menuUser.addEventListener('blur', () => {
-  menuUser.classList.remove('show')
-  main.style.pointerEvents = 'initial'
-  main.style.filter = ''
-  main.style.backgroundColor = 'initial'
-})
+menuUser.addEventListener("blur", () => {
+  menuUser.classList.remove("show");
+  main.style.pointerEvents = "initial";
+  main.style.filter = "";
+  main.style.backgroundColor = "initial";
+});
+
+changeTheme.addEventListener("click", () => {
+  toggleTheme();
+});
+
+const toggleTheme = () => {
+  if (main.classList.contains("dark")) {
+    lightMode.style.display = "flex";
+    darkMode.style.display = "none";
+    localStorage.setItem("temaPreferido", "dark");
+  } else {
+    lightMode.style.display = "none";
+    darkMode.style.display = "flex";
+    localStorage.setItem("temaPreferido", "light");
+  }
+
+  console.log(localStorage.getItem("temaPreferido"));
+  main.classList.toggle("dark");
+};
